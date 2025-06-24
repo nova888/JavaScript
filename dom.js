ab9dba1cdb6d1b132bf33447a4c570f704ed378d
nova888 <nova.tampati@gmail.com>
2025-06-24 15:38:33 +0700

    percobaan latihan javascript

---
+++ dom.js
@@ -0,0 +1,20 @@
+document.title = 'Nova Tampati'
+const body = document.body
+
+body.append('HELLO WORLD')
+
+const h1 = document.createElement('h1')
+h1.innerText = "sikat"
+body.append(h1)
+
+
+const namaSaya = document.createElement('p')
+namaSaya.innerHTML = '<marquee>Nova Tampati</marquee>'
+
+body.append(namaSaya)
+
+
+
+const namaKamu = document.createElement('b')
+namaKamu.textContent = '<marquee>anjay saya bro</marquee>'
+body.append(namaKamu)
---
+++ main.js
@@ -0,0 +1,17 @@
+let nama = 'Dea Afrizal'
+let usia = 30
+let tinggiBadan = 173.5 
+let beratBadan
+let pacar = null
+
+if (pacar == null) {
+    pacar = 'Belum punya'
+} else {
+    pacar = 'udah punya'
+}
+
+
+alert(
+    'nama saya ${nama} usia saya ${usia} tinggi bandan saya ${tinggiBadan} berat badan saya adalah ${beratBadan} dan pacar saya ${pacar}',
+
+)
