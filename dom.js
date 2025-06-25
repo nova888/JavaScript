9bbd5d9ed9239a14e3da998c1df26c8f054d2fec
nova888 <nova.tampati@gmail.com>
2025-06-25 13:52:50 +0700

    tambahan gj

--- dom.js
+++ dom.js
@@ -1,20 +1,31 @@
 document.title = 'Nova Tampati'
-const body = document.body
+// const body = document.body
 
-body.append('HELLO WORLD')
+// body.append('HELLO WORLD')
 
-const h1 = document.createElement('h1')
-h1.innerText = "sikat"
-body.append(h1)
-
-
-const namaSaya = document.createElement('p')
-namaSaya.innerHTML = '<marquee>Nova Tampati</marquee>'
-
-body.append(namaSaya)
+// const h1 = document.createElement('h1')
+// h1.innerText = "sikat"
+// body.append(h1)
 
 
+// const namaSaya = document.createElement('p')
+// namaSaya.innerHTML = '<marquee>Nova Tampati</marquee>'
 
-const namaKamu = document.createElement('b')
-namaKamu.textContent = '<marquee>anjay saya bro</marquee>'
-body.append(namaKamu)
+// body.append(namaSaya)
+
+
+
+// const namaKamu = document.createElement('b')
+// namaKamu.textContent = '<marquee>anjay saya bro</marquee>'
+// body.append(namaKamu)
+
+
+// Data Selector
+const btn1 = document.getElementById('btn1')
+const btn2 = document.getElementById('btn2')
+
+
+btn1.style.border = 'none'
+btn1.style.padding = '8px'
+btn1.style.fontSize = '24px'
+btn1.style.background = 'tomato'
