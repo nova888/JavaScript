eb12268ccd37686dc65b451c7cb986500c1187a8
nova888 <nova.tampati@gmail.com>
2025-06-28 22:29:54 +0700

    ngelu

--- dom.js
+++ dom.js
@@ -1,5 +1,5 @@
 document.title = 'Nova Tampati'
-// const body = document.body
+const body = document.body
 
 // body.append('HELLO WORLD')
 
@@ -21,27 +21,59 @@ document.title = 'Nova Tampati'
 
 
 // Data Selector
-const btn1 = document.getElementById('btn1')
-const btn2 = document.querySelector('.btn2 ')
+// const btn1 = document.getElementById('btn1')
+// const btn2 = document.getElementById('.btn2 ')
 
-const defaultText = 'Klik Saya 1'
-btn1.textContent = defaultText
+// const defaultText = 'Klik Saya 1'
+// btn1.textContent = defaultText
 
+// btn1.style.border = 'none'
+// btn1.style.padding = '8px'
+// btn1.style.fontSize = '24px'
+// btn1.style.background = 'tomato'
+
+// function clickButton() {
+
+// btn1.style.background = 'aqua'
+// const newText = document.createElement('p')
+// newText.textContent = "HALO BUNG! APA KABAR"
+// body.append(newText)
+// }
+
+// function ubahText() {
+//    btn1.textContent = 'sudah cok'
+// }
+
+// function oriText() {
+//     btn1.textContent = defaultText
+// }
+
+// const defaultText2 = 'Klik Saya 2'
+// btn1.textContent = defaultText2
+
+// function keluar () {
+//     const keluarText = document.createElement('p')
+//     newText.textContent = "SUDAH TERUBAH YA"
+//     body.append (ubahText)
+// }
+
+//  const btn1 = document.querySelector(btn1)
+
+btn1.textContent = "Klik Saya"
 btn1.style.border = 'none'
 btn1.style.padding = '8px'
 btn1.style.fontSize = '24px'
 btn1.style.background = 'tomato'
 
-function gantiWarna() {
-
-btn1.style.background = 'aqua'
+function keluarText() {
+    const newText = document.createElement('p')
+    newText.style.color = 'black'
+    newText.textContent = "hallo bung"
+    body.append(newText)
 }
 
-
-function ubahText() {
-   btn1.textContent = 'sudah cok'
-}
-
-function oriText() {
-    btn1.textContent = defaultText
+function ubahWarna () {
+    if(newText) {
+    newText.style.color = 'aqua'
+    }
 }
