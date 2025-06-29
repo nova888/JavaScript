fc6c45afed10909792bbd944b55c69086a7c5904
nova888 <nova.tampati@gmail.com>
2025-06-30 03:35:02 +0700

    tambah script

--- array.js
+++ array.js
@@ -3,4 +3,17 @@
 // const varrrrr = ['alalala', function() {return true}, {x: "trueee"}]
 
 const arraySaya = ['😊', '💋', '🤞']
-console.log(arraySaya)
+// console.log(arraySaya)
+
+const arrayKamu = []
+arrayKamu[0] = '🌹'
+arrayKamu[1] = '🐱‍🐉'
+arrayKamu[2] = '👀'
+// console.log(arrayKamu)
+
+const arrayKita = new Array()
+arrayKita[0] = '🤣'
+arrayKita [3] = '🐱‍🏍'
+arrayKita[1] = '😍'
+arrayKita[2] = '🎂'
+console.log(arrayKita[0])
