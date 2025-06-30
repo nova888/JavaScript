cb5915f3794655f36d6de857a2355d494310c5aa
nova888 <nova.tampati@gmail.com>
2025-06-30 12:32:55 +0700

    tambah

--- array.js
+++ array.js
@@ -2,18 +2,27 @@
 
 // const varrrrr = ['alalala', function() {return true}, {x: "trueee"}]
 
-const arraySaya = ['😊', '💋', '🤞']
+const arraySaya = ['🥑', '🍑', '🍐','🍉', '🍊']
 // console.log(arraySaya)
 
-const arrayKamu = []
-arrayKamu[0] = '🌹'
-arrayKamu[1] = '🐱‍🐉'
-arrayKamu[2] = '👀'
+// const arrayKamu = []
+// arrayKamu[0] = '🌹'
+// arrayKamu[1] = '🐱‍🐉'
+// arrayKamu[2] = '👀'
 // console.log(arrayKamu)
 
-const arrayKita = new Array()
-arrayKita[0] = '🤣'
-arrayKita [3] = '🐱‍🏍'
-arrayKita[1] = '😍'
-arrayKita[2] = '🎂'
-console.log(arrayKita[0])
+// const arrayKita = new Array()
+// arrayKita[0] = '🤣'
+// arrayKita [3] = '🐱‍🏍'
+// arrayKita[1] = '😍'
+// arrayKita[2] = '🎂'
+// console.log(arrayKita[0])
+
+const semangka = arraySaya.includes ('🍉')
+const posisiSemangka = arraySaya.indexOf('🍉')
+
+if (semangka) {
+    console.log('buah semangka itu ada dan dia berada di posisi index ke - ')(posisiSemangka)
+} else {
+    console.log('saya tidak tahu posisinya dimana')
+}
